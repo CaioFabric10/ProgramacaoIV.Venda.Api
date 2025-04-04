cb7a8475acd2497f31c6f54d134a20ed767027cc
CaioFabric10 <caiofabricio07@gmail.com>
2025-04-04 18:33:01 -0300

    Adicionar arquivos de projeto.

---
+++ ProgramacaoIV.Venda.Api/Migrations/20250404201002_InitComVendedor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProgramacaoIV.Venda.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitComVendedor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vendedor",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "char(36)", nullable: false),
+                    NM_VENDEDOR = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    DS_EMAIL = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    IN_ATIVO = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    DT_CRIACAO = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DT_ATUALIZACAO = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendedor", x => x.ID);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vendedor");
+        }
+    }
+}
---
+++ ProgramacaoIV.Venda.Api/Migrations/20250404202525_AddIdVendedorNaTransacao.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProgramacaoIV.Venda.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdVendedorNaTransacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ID_VENDEDOR",
+                table: "TRANSACAO",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TRANSACAO_ID_VENDEDOR",
+                table: "TRANSACAO",
+                column: "ID_VENDEDOR");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TRANSACAO_Vendedor_ID_VENDEDOR",
+                table: "TRANSACAO",
+                column: "ID_VENDEDOR",
+                principalTable: "Vendedor",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TRANSACAO_Vendedor_ID_VENDEDOR",
+                table: "TRANSACAO");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TRANSACAO_ID_VENDEDOR",
+                table: "TRANSACAO");
+
+            migrationBuilder.DropColumn(
+                name: "ID_VENDEDOR",
+                table: "TRANSACAO");
+        }
+    }
+}
